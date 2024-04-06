eee3d304efcd533ef864a9e7a4fb0a6054593b45
Oleksandra Biskulova <alexandrabiskulova@gmail.com>
2024-04-06 16:01:50 +0200

    Improvements to AuthResponse added.

--- Sources/LocationTracker/Model/AuthResponse.swift
+++ Sources/LocationTracker/Model/AuthResponse.swift
@@ -8,33 +8,35 @@
 import Foundation
 
 struct AuthResponse: Decodable {
-    var token: Token
+    let accessToken: String
+    let expiresAt: Date
+    let refreshToken: String
+
+    let token: Token
     
     private enum CodingKeys: String, CodingKey {
         case accessToken
-        case expiresAt //"2024-04-04T10:53:23.345Z"
+        case expiresAt
         case refreshToken
     }
+        
+    private let dateTimeFormatter : DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        return formatter
+    }()
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
-        let accessToken = try container.decode(String.self, forKey: .accessToken)
-        let refreshToken = try container.decode(String.self, forKey: .refreshToken)
+        self.accessToken = try container.decode(String.self, forKey: .accessToken)
+        self.refreshToken = try container.decode(String.self, forKey: .refreshToken)
         let expirationDate = try container.decode(String.self, forKey: .expiresAt)
-        
-        let expiresIn: Int
-        if let expiresInString = try? container.decode(String.self, forKey: .expiresAt) {
-            guard let value = Int(expiresInString) else {
-                throw ResponseError.parsingError
-            }
-            expiresIn = value
-        } else {
-            expiresIn = try container.decode(Int.self, forKey: .expiresAt)
-        }
+        self.expiresAt = dateTimeFormatter.date(from: expirationDate) ?? Date()
 
-        let expirationDate2 = Date(timeIntervalSinceNow: Double(expiresIn))
-        token = Token(accessToken: accessToken, refreshToken: refreshToken, expirationDate: expirationDate2)
+        self.token = Token(accessToken: accessToken, refreshToken: refreshToken, expirationDate: expiresAt)
     }
 }
 
